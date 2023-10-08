f7ed93f3f264881deda1878fc5a09a779b3784e3
Sean Miller <sean@seanmiller.ca>
2023-10-08 16:28:34 -0400

    Initial lib/ and spec/

---
+++ lib/service.rb
@@ -0,0 +1,12 @@
+class Service
+
+  def my_hounds
+    {
+      breeding_stock: "Spartan",
+      traits: {
+        flewed_as: "Spartan",
+        sanded_as: "Spartan"
+      }
+    }
+  end
+end
---
+++ spec/with_requirement_spec.rb
@@ -0,0 +1,41 @@
+require "service"
+
+RSpec.describe "with requirement" do
+
+  subject { Service.new.my_hounds }
+
+  context "my_hounds" do
+    describe "keys in response" do
+      # @REQUIREMENT: my_hounds includes :breeding_stock
+      it "includes :breeding_stock" do
+        expect(subject).to include :breeding_stock
+      end
+
+      # @REQUIREMENT: my_hounds includes :traits
+      it "includes :traits" do
+        expect(subject).to include :traits
+      end
+    end
+  end
+
+  context "using my_hounds" do
+    describe "for one thing" do
+      # @RELIES_ON: my_hounds includes :breeding_stock
+      it "does something with :breeding_stock" do
+        expect(subject[:breeding_stock]).to eq("Spartan")
+      end
+
+      # @RELIES_ON: my_hounds includes :traits
+      it "does something with :traits" do
+        expect(subject[:traits][:flewed_as]).to eq("Spartan")
+      end
+    end
+
+    describe "for another thing" do
+      # @RELIES_ON: my_hounds includes :breeding_stock
+      it "checks something else" do
+        expect(subject[:breeding_stock]).not_to eq("Athenian")
+      end
+    end
+  end
+end
